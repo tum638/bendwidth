7359c79151c25b2e864728b8da017a6212bc8227
Tanatswa638 <135277949+Tanatswa638@users.noreply.github.com>
2024-01-19 00:37:53 -0500

    fixed screen sharing feature

--- frontend/src/videoComponents/ShareScreenButton.js
+++ frontend/src/videoComponents/ShareScreenButton.js
@@ -5,34 +5,9 @@ import pair from "../redux-elements/pair";
 const ShareScreenButton = () => {
     const callStatus = useSelector(state => state.callStatus);
     const dispatch = useDispatch();
-
-    const toggleShareScreen = async () => {
-        let mediaStream;
-        if (!callStatus.sharingScreen) {
-            const options = { video: true, audio: false, surfaceSwitching: 'include', cursor: true };
-            try {
-                mediaStream = await navigator.mediaDevices.getDisplayMedia(options);
-                setNewVideoStream(mediaStream);
-                dispatch(updateCallStatus(pair('sharingScreen', true)));
-            } catch (error) {
-                console.log(error);
-            }
-        } else {
-            const options = { video: true, audio: true };
-            try {
-                mediaStream = await navigator.mediaDevices.getUserMedia(options);
-                setNewVideoStream(mediaStream);
-                dispatch(updateCallStatus(pair('sharingScreen', false)));
-            } catch (error) {
-                console.log(error);
-            }    
-        }
-    }
-
+  
     // replace the current tracks with required tracks.
-    const setNewVideoStream = (mediaStream) => {
-        dispatch(updateCallStatus(pair('localStream', mediaStream)));
-        const [videoTrack] = mediaStream.getVideoTracks();
+    const setNewVideoStream = (videoTrack) => {
         const senders = callStatus.peerConnection.getSenders();
         const sender = senders.find(s => {
             if (s.track) {
@@ -43,7 +18,37 @@ const ShareScreenButton = () => {
         });
         sender.replaceTrack(videoTrack);
     }
-    return (<div className='button-wrapper middle' onClick={toggleShareScreen}>
+    const startSharing = async () => {
+        let mediaStream;
+        if (!callStatus.sharingScreen) {
+            let options = { video: true, audio: false, surfaceSwitching: 'include', cursor: true };
+            try {
+                mediaStream = await navigator.mediaDevices.getDisplayMedia(options);
+                const [videoTrack] = mediaStream.getVideoTracks();
+
+                // listen to stop of screen share.
+                videoTrack.onended = async () => {
+                    let options = { video: true, audio: true };
+                    try {
+                        mediaStream = await navigator.mediaDevices.getUserMedia(options);
+                        const [videoTrack] = mediaStream.getVideoTracks();
+                        setNewVideoStream(videoTrack);
+                        dispatch(updateCallStatus(pair('sharingScreen', false)));
+                        dispatch(updateCallStatus(pair('localStream', mediaStream)));
+                    } catch (error) {
+                        console.log(error);
+                    }
+                    
+                }
+                setNewVideoStream(videoTrack);
+                dispatch(updateCallStatus(pair('sharingScreen', true)));
+                dispatch(updateCallStatus(pair('localStream', mediaStream)));
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+    return (<div className='button-wrapper middle' onClick={startSharing}>
         <i className={`fa fa-desktop`}></i>
         <div className="btn-text">{callStatus.sharingScreen ? "Stop Sharing" : "Share Screen"}</div>
             </div>)
