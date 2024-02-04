f18be4770cf4310c65f312835b1ba9c0bc7a9f73
Vento24 <mvento@kountable.com>
2024-02-03 22:07:32 -0500

    avatar v1

--- frontend/src/components/main/MainContent.jsx
+++ frontend/src/components/main/MainContent.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import AiAvatars from "../../ai-avatars/streaming_avators";
 
 const MainContent = () => {
-  return <div>MainContent</div>;
+  return <div><h1>MainContent </h1>
+   
+      <AiAvatars/>
+
+  </div>;
 };
 
 export default MainContent;
