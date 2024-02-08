3f479d4b24608256c6dc15707414b7fa56fe7af7
Tanatswa638 <135277949+Tanatswa638@users.noreply.github.com>
2024-02-07 22:53:01 -0500

    lessended time limit

--- django-backend/user/management/commands/runapscheduler.py
+++ django-backend/user/management/commands/runapscheduler.py
@@ -47,7 +47,7 @@ class Command(BaseCommand):
 
     scheduler.add_job(
       my_job,
-      trigger=CronTrigger(second="*/5"),  # Every 5 seconds
+      trigger=CronTrigger(second="*/10"),  # Every 10 seconds
       id="my_job",  # The `id` assigned to each job MUST be unique
       max_instances=1,
       replace_existing=True,
